be0f09a3816be71032f28ed076acbd35ff700f86
Pat8998 <jean.michel.lesatanique@gmail.com>
2025-11-28 23:27:22 +0100

    function possiblity

--- main.lua
+++ main.lua
@@ -2,6 +2,14 @@ local t = 2
 local timetrans = 2
 local tTrans = 1
 local isBlack = false
+local func = {
+	function(x) return 0  end,
+	function(x) half = love.graphics.getHeight()/40 return math.fmod(x, half*2) - half end,
+	function(x) return math.fmod(x, love.graphics.getHeight()/40) end,
+	function(x, y) return x^y end,
+	function(x, y) return math.sin(x/25 + y)* 0.5*25 end,
+	number = 1
+}
 local Circle = false
 
 function love.load()
@@ -28,6 +36,31 @@ function love.draw()
 	love.graphics.printf(os.date("%H:%M:%S"), 0, love.graphics.getHeight()/2, love.graphics.getWidth()/10, 'center', 0, 10)
 	love.graphics.setColor(0, 0, 0, tTrans)
 	love.graphics.printf(t, 0, 0, love.graphics.getWidth()/3, 'right', 0, 3)
+
+	--FUNCTION
+	local time = love.timer.getTime() * t
+	love.graphics.setLineWidth(20)
+	if func.number ~= 1 then
+	for i = 1, love.graphics.getWidth() do
+		local j = math.sin(time)
+		love.graphics.setColor(
+			math.sin(time   ) * 0.5 + 0.5,
+			math.sin(time   +math.pi/3 * i) * 0.5 + 0.5,
+			math.sin(time   +2*math.pi/3 * i) * 0.5 + 0.5,
+			 0.5)
+		love.graphics.line(
+			i,
+			func[func.number ]( i, j  ) * 20 + love.graphics.getHeight()/2,
+			i +1,
+			func[func.number ]( i+1, j  ) * 20 + love.graphics.getHeight()/2)
+		if func.number == 3 then  
+			love.graphics.line(
+			i,
+			func[func.number ]( i, j  ) * 20,
+			i +1,
+			func[func.number ]( i+1, j  ) * 20 )end
+	end end
+
 	if Circle then
 		for i = 1, 100, 1 do
 			local time = love.timer.getTime() * t
@@ -68,5 +101,9 @@ function love.keypressed(key)
 		isBlack = not isBlack
 	elseif key == 'c' then
 		Circle = not Circle
+	elseif key == 'f' then
+		func.number = func.number + 1
+		if func.number > #func then func.number = 1 end
+		print("Function changed to "..func.number)
 	end
 end
